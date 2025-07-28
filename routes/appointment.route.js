54215482021c92e74e1b1a8b6ee1444d2251efc6
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-07-28 20:06:39 +0300

    fixed date format and added notes to appointments

--- routes/appointment.route.js
+++ routes/appointment.route.js
@@ -45,7 +45,7 @@ router.get("/:id", async (req, res) =>
 {
     try
     {
-        const foundAppointment = await Appointment.findById(req.params.id);
+        const foundAppointment = await Appointment.findById(req.params.id).populate("doctor");
         res.render("appointments/appointment-details.ejs", {foundAppointment});
     }
     catch(error)
@@ -55,11 +55,14 @@ router.get("/:id", async (req, res) =>
 });
 
 
-router.post("/:id/note", async (req, res) =>
+router.put("/:id", async (req, res) =>
 {
     try
     {
-
+        let updatedAppointment = await Appointment.findById(req.params.id);
+        const note = req.body;
+        updatedAppointment.notes.push(note);
+        updatedAppointment.save();
         res.redirect("/appointments/" + req.params.id);
     }
     catch(error)
@@ -68,5 +71,16 @@ router.post("/:id/note", async (req, res) =>
     }
 });
 
+router.delete("/:id", async (req, res) =>
+{
+    try
+    {
+        await Appointment.findByIdAndDelete(req.params.id);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+});
 
 module.exports = router;
